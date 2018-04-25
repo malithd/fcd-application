775ddecaee7be7caf68198325c1b2afe0e3d2094
malithd <mmalithh@gmail.com>
2018-04-25 09:23:54 +0200

    Adding arguments to execute producer

--- src/main/java/org/tum/idp/fcd/FlinkFcdEventProducer.java
+++ src/main/java/org/tum/idp/fcd/FlinkFcdEventProducer.java
@@ -1,5 +1,6 @@
 package org.tum.idp.fcd;
 
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -10,10 +11,19 @@ public class FlinkFcdEventProducer {
     private static final String KAFKA_BROKER = "localhost:9092";
 
     public static void main(String[] args) throws Exception {
+        ParameterTool params = ParameterTool.fromArgs(args);
+        if (params.getNumberOfParameters() < 2) {
+            throw new IllegalArgumentException("Must have either 'appid' or 'appcode' or 'bbox' as first argument. \n");
+        }
+
+        String appId = params.getRequired("app_id");
+        String appCode = params.getRequired("app_code");
+        String bboxInput = params.get("bbox");
+        String bbox = (bboxInput == null) ? "48.160250,11.551678;48.159462,11.558652" : bboxInput;
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
-        DataStream<FcdEvent> eventStream = env.addSource(new FcdEventSource("https://traffic.cit.api.here.com/traffic/6.2/flow.json?app_id=foo&app_code=bar&bbox=48.160250,11.551678;48.159462,11.558652"));
+        DataStream<FcdEvent> eventStream = env.addSource(new FcdEventSource("https://traffic.cit.api.here.com/traffic/6.2/flow.json?app_id=" + appId + "&app_code=" + appCode + "&bbox=" + bbox));
         //TODO:appid and appcde and bbx coordinates input and tmc data ingestion
         //taxiEventStream.print();
         FlinkKafkaProducer010<FcdEvent> producer = new FlinkKafkaProducer010<>(
